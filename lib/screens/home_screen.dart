3d0168f7661b3e6ac48ad4187e673a218ffc96e0
Muhammad Awais Yaseen <85793305+muhammadawaisyaseen@users.noreply.github.com>
2022-12-29 23:59:11 +0500

    uiu

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          color: Color(0xFF1a1e3f),
+        ),
+        child: Column(
+          children: [
+            Container(
+              height: MediaQuery.of(context).size.height * 0.5,
+              decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                      colors: [Color(0xFFe7e7e7), Color(0xFFffffff)],
+                      stops: [0.3, 0.9]),
+                  borderRadius:
+                      BorderRadius.only(bottomRight: Radius.circular(80))),
+            ),
+            Container(
+                height: MediaQuery.of(context).size.height * 0.5,
+                decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                        colors: [Color(0xFFe7e7e7), Color(0xFFffffff)],
+                        stops: [0.3, 0.9]),
+                    borderRadius:
+                        BorderRadius.only(bottomRight: Radius.circular(80))),
+                child: Container(
+                  height: MediaQuery.of(context).size.height * 0.5,
+                  decoration: const BoxDecoration(
+                      color: Color(0xFF1a1e3f),
+                      borderRadius:
+                          BorderRadius.only(topLeft: Radius.circular(60))),
+                child: Padding(
+                padding: EdgeInsets.all(20),
+                child: Column(
+                  children: [
+                    RichText(text: TextSpan(
+                      text: 
+                    ))
+                  ],
+                ),
+                ),
+                )
+                )
+          ],
+        ),
+      ),
+        );
+  }
+}
