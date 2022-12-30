bcbf09d4d93221345243215023ccff7dd75961e8
Muhammad Awais Yaseen <85793305+muhammadawaisyaseen@users.noreply.github.com>
2022-12-30 17:31:33 +0500

    home pay half done

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -22,6 +22,7 @@ class HomePage extends StatelessWidget {
                       stops: [0.3, 0.9]),
                   borderRadius:
                       BorderRadius.only(bottomRight: Radius.circular(80))),
+              child: Image.asset('assets/images/homephoto.png'),
             ),
             Container(
                 height: MediaQuery.of(context).size.height * 0.5,
@@ -33,25 +34,50 @@ class HomePage extends StatelessWidget {
                         BorderRadius.only(bottomRight: Radius.circular(80))),
                 child: Container(
                   height: MediaQuery.of(context).size.height * 0.5,
+                  width: MediaQuery.of(context).size.width * 1,
                   decoration: const BoxDecoration(
                       color: Color(0xFF1a1e3f),
                       borderRadius:
                           BorderRadius.only(topLeft: Radius.circular(60))),
-                child: Padding(
-                padding: EdgeInsets.all(20),
-                child: Column(
-                  children: const[
-                    RichText(text: TextSpan(
-                      text: 
-                    ))
-                  ],
-                ),
-                ),
-                )
-                )
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(
+                            top: 60, bottom: 30, left: 30, right: 40),
+                        child: RichText(
+                            text: const TextSpan(
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 32),
+                                children: [
+                              TextSpan(
+                                text: "It's time for",
+                              ),
+                              TextSpan(
+                                text: '\n',
+                              ),
+                              TextSpan(
+                                text: 'Winter Shopping!',
+                              ),
+                              TextSpan(
+                                text: '\n\n',
+                              ),
+                              TextSpan(
+                                  text:
+                                      'Lets purchase the good quality cloths from here. You will be happy and satisfied after shopping!',
+                                  style: TextStyle(
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.normal)),
+                            ])),
+                      ),
+                    ],
+                  ),
+                ))
           ],
         ),
       ),
-        );
+    );
   }
 }
