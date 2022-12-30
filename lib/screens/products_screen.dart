925bb6952c2375867266cefd81385cb46ffd7af8
Muhammad Awais Yaseen <85793305+muhammadawaisyaseen@users.noreply.github.com>
2022-12-31 03:12:55 +0500

    Tab Bar

--- lib/screens/products_screen.dart
+++ lib/screens/products_screen.dart
@@ -8,112 +8,134 @@ class ProductsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(
-        body: Container(
-          color: Colors.white,
-          child: Column(
-            children: [
-              // 1st Child
-              Container(
-                width: double.infinity,
-                height: MediaQuery.of(context).size.height * 0.25,
-                decoration: const BoxDecoration(
-                    color: Color(0xFF1a1e3f),
-                    borderRadius:
-                        BorderRadius.only(bottomLeft: Radius.circular(90))),
-                child: Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 20, vertical: 26),
-                      child: Row(
-                        children: [
-                          Container(
-                            width: 50,
-                            height: 50,
-                            decoration: BoxDecoration(
-                                color: const Color(0xFFf3c9ca),
-                                border: Border.all(color: Colors.pink),
-                                shape: BoxShape.circle,
-                                image: const DecorationImage(
-                                    image:
-                                        AssetImage('assets/images/awais.jpg'),
-                                    fit: BoxFit.fitWidth)),
-                          ),
-                          const SizedBox(
-                            width: 10,
-                          ),
-
-                          ///
-                          RichText(
-                              text: const TextSpan(
-                                  style: TextStyle(
-                                      color: Colors.white,
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 18),
-                                  children: [
-                                TextSpan(
-                                  text: "Muhammad Awais",
-                                ),
-                                TextSpan(
-                                  text: '\n',
-                                ),
-                                TextSpan(
-                                    text: '@malikawais',
-                                    style: TextStyle(
-                                        color: Colors.grey,
-                                        fontWeight: FontWeight.normal,
-                                        fontSize: 14)),
-                              ])),
-                          const Spacer(),
-                          Container(
-                            width: 50,
-                            height: 50,
-                            decoration: const BoxDecoration(
-                                // color: Colors.black,
-                                ),
-                            child: Row(
-                              children: [
-                                const Icon(
-                                  Icons.shopping_bag_rounded,
-                                  color: Colors.white,
-                                ),
-                                Container(
-                                  width: 3,
-                                  height: 3,
-                                  decoration: const BoxDecoration(
-                                      color: Colors.pink,
-                                      shape: BoxShape.circle),
-                                )
-                              ],
+      child: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          body: Container(
+            color: Colors.white,
+            child: Column(
+              children: [
+                // 1st Child
+                Container(
+                  width: double.infinity,
+                  height: MediaQuery.of(context).size.height * 0.25,
+                  decoration: const BoxDecoration(
+                      color: Color(0xFF1a1e3f),
+                      borderRadius:
+                          BorderRadius.only(bottomLeft: Radius.circular(90))),
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 20, vertical: 26),
+                        child: Row(
+                          children: [
+                            Container(
+                              width: 50,
+                              height: 50,
+                              decoration: BoxDecoration(
+                                  color: const Color(0xFFf3c9ca),
+                                  border: Border.all(color: Colors.pink),
+                                  shape: BoxShape.circle,
+                                  image: const DecorationImage(
+                                      image:
+                                          AssetImage('assets/images/awais.jpg'),
+                                      fit: BoxFit.fitWidth)),
                             ),
-                          ),
-                          // Tab Bar
-                        ],
+                            const SizedBox(
+                              width: 10,
+                            ),
+
+                            ///
+                            RichText(
+                                text: const TextSpan(
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 18),
+                                    children: [
+                                  TextSpan(
+                                    text: "Muhammad Awais",
+                                  ),
+                                  TextSpan(
+                                    text: '\n',
+                                  ),
+                                  TextSpan(
+                                      text: '@malikawais',
+                                      style: TextStyle(
+                                          color: Colors.grey,
+                                          fontWeight: FontWeight.normal,
+                                          fontSize: 14)),
+                                ])),
+                            const Spacer(),
+                            Container(
+                              width: 50,
+                              height: 50,
+                              decoration: const BoxDecoration(
+                                  // color: Colors.black,
+                                  ),
+                              child: Row(
+                                children: [
+                                  const Icon(
+                                    Icons.shopping_bag_rounded,
+                                    color: Colors.white,
+                                  ),
+                                  Container(
+                                    width: 3,
+                                    height: 3,
+                                    decoration: const BoxDecoration(
+                                        color: Colors.pink,
+                                        shape: BoxShape.circle),
+                                  )
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
-
-                    )
-                  ],
+                      // Tab Bar
+                      Container(
+                        // margin: EdgeInsets.all(10),
+                        width: 350,
+                        height: 60,
+                        decoration: BoxDecoration(
+                            color: Color(0xFF282d54),
+                            borderRadius: BorderRadius.circular(16)),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: TabBar(
+                              indicator: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(16),
+                                  color: Color(0xFF1b1e3f)),
+                              tabs: [
+                                Text('Colleagues'),
+                                Text('Collection'),
+                                Text('My Award')
+                              ]),
+                        ),
+                      )
+                    ],
+                  ),
                 ),
-              ),
-              //2nd Child
+                //2nd Child
 
-              Container(
-                width: double.infinity,
-                height: MediaQuery.of(context).size.height * 0.71,
-                decoration: const BoxDecoration(
-                  color: Color(0xFF1a1e3f),
-                ),
-                child: Container(
+                Container(
                   width: double.infinity,
                   height: MediaQuery.of(context).size.height * 0.71,
                   decoration: const BoxDecoration(
-                      color: Colors.white,
-                      borderRadius:
-                          BorderRadius.only(topRight: Radius.circular(90))),
+                    color: Color(0xFF1a1e3f),
+                  ),
+                  child: Container(
+                    width: double.infinity,
+                    height: MediaQuery.of(context).size.height * 0.71,
+                    decoration: const BoxDecoration(
+                        color: Colors.white,
+                        borderRadius:
+                            BorderRadius.only(topRight: Radius.circular(90))),
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
