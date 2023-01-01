ae12635c960515dbc169706e6410ba3440be371f
Muhammad Awais Yaseen <85793305+muhammadawaisyaseen@users.noreply.github.com>
2023-01-01 20:30:55 +0500

    product name

--- lib/screens/tabs/colleagues_tab.dart
+++ lib/screens/tabs/colleagues_tab.dart
@@ -5,27 +5,54 @@ import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 class ColleagueTab extends StatelessWidget {
-  const ColleagueTab({super.key});
+  ColleagueTab({super.key});
+
+  List<Color> tileColor = [
+    Color(0xFFffeee6),
+    Color(0xFFeafffc),
+    Color(0xFFfff0f8),
+    Color(0xFFfffbe8),
+    Color.fromARGB(255, 194, 210, 208),
+    Color.fromARGB(139, 229, 232, 238),
+  ];
+  List<String> productPhoto = [
+    'assets/images/shirt.png',
+    'assets/images/pent.png',
+    'assets/images/kids kameez.png',
+    'assets/images/kids pentshirt.png',
+    'assets/images/shalwar kameez.png',
+    'assets/images/sharwani.png'
+  ];
+  List<String> productName = [
+    'Half sleaves t-shirt',
+    'Men jeans Pent',
+    'Red cotton kids kameez',
+    'kids pentshirt',
+    'Men white shalwar kameez',
+    'Marriage sharwani'
+  ];
+  List<int> productPrice = [
+    130,
+    270,
+    410,
+    699,
+    371,
+    765,
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(left: 20, right: 20, top: 20),
       child: StaggeredGridView.countBuilder(
-        // shrinkWrap: true,
         staggeredTileBuilder: (index) =>
-            StaggeredTile.count(3, index.isOdd ? 4 : 5),
-        itemCount: 8,
-        // mainAxisExtent: 250,
+            StaggeredTile.count(3, index.isEven ? 5.3 : 4),
+        itemCount: 6,
         crossAxisCount: 6,
-
-        ///
         crossAxisSpacing: 12,
         mainAxisSpacing: 14,
-
         itemBuilder: (context, index) {
           return Container(
-            // height: 300,
             decoration: BoxDecoration(
               color: Colors.white,
               // color: Colors.yellow,
@@ -37,12 +64,11 @@ class ColleagueTab extends StatelessWidget {
                 //product and fav icon
                 Container(
                   decoration: BoxDecoration(
-                      color: Colors.pink[200],
+                      // color: Colors.pink[200],
+                      color: tileColor[index],
                       borderRadius: BorderRadius.circular(10)),
                   width: double.infinity,
-                  height: index.isOdd
-                      ? 200
-                      : 270, // (mainAxisExtent: 200 Total height of gridtile)
+                  height: index.isEven ? 270 : 200,
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.end,
                     children: [
@@ -63,9 +89,10 @@ class ColleagueTab extends StatelessWidget {
                       ),
                       Center(
                         child: Image.asset(
-                          'assets/images/shirt.png',
-                          // width: double.infinity,
-                          // height: 130,
+                          // 'assets/images/pent.png',
+                          productPhoto[index],
+                          width: double.infinity,
+                          height: index.isOdd ? 130 : 200,
                           fit: BoxFit.cover,
                         ),
                       ),
@@ -74,12 +101,10 @@ class ColleagueTab extends StatelessWidget {
                 ),
 
                 //price and Name
-                // const SizedBox(
-                //   height: 6,
-                // ),
+
                 Spacer(),
-                const Text(
-                  'Half sleaves t-Shirt',
+                Text(
+                  productName[index],
                   style: TextStyle(
                       color: Colors.black54,
                       fontWeight: FontWeight.bold,
@@ -89,11 +114,13 @@ class ColleagueTab extends StatelessWidget {
                   height: 2,
                 ),
                 RichText(
-                    text: const TextSpan(
+                    text: TextSpan(
                         style: TextStyle(color: Colors.grey, fontSize: 14),
                         children: [
                       TextSpan(
-                          text: "Rs.887", style: TextStyle(color: Colors.pink)),
+                          // Text('Indian currency is ${currency}');
+                          text: '\$ ${productPrice[index].toString()}',
+                          style: TextStyle(color: Colors.pink)),
                       TextSpan(
                         text: '  ',
                       ),
@@ -101,6 +128,7 @@ class ColleagueTab extends StatelessWidget {
                         text: '20% off',
                       ),
                     ])),
+                ///////////////////////
               ],
             ),
           );
